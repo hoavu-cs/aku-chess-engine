c1894e3871df3a4ad5947f723993580543cbb811
Hoa Vu <hvutrong@gmail.com>
2025-01-25 20:58:14 -0800

    temporary disable futility pruning

--- src/search.cpp
+++ src/search.cpp
@@ -432,19 +432,19 @@ int alphaBeta(Board& board,
     }
 
     // Futility pruning
-    // const int futilityMargin = 350;
-    // if (depth == 1 && !board.inCheck() && !endGameFlag && !leftMost) {
-    //     int standPat = evaluate(board);
-    //     if (whiteTurn) {
-    //         if (standPat + futilityMargin < alpha) {
-    //             return alpha;
-    //         }
-    //     } else {
-    //         if (standPat - futilityMargin > beta) {
-    //             return beta;
-    //         }
-    //     }
-    // }
+    const int futilityMargin = 350;
+    if (depth == 1 && !board.inCheck() && !endGameFlag && !leftMost) {
+        int standPat = evaluate(board);
+        if (whiteTurn) {
+            if (standPat + futilityMargin < alpha) {
+                return standPat + futilityMargin;
+            }
+        } else {
+            if (standPat - futilityMargin > beta) {
+                return standPat - futilityMargin;
+            }
+        }
+    }
 
     std::vector<std::pair<Move, int>> moves = prioritizedMoves(board, depth, previousPV, leftMost);
     int bestEval = whiteTurn ? -INF : INF;
