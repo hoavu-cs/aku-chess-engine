e76beea967d6c830bbf562b17e3485ed6354a730
Hoa Vu <hvutrong@gmail.com>
2025-02-16 18:28:17 -0800

    refine time control

--- src/search.cpp
+++ src/search.cpp
@@ -190,7 +190,7 @@ int lateMoveReduction(Board& board, Move move, int i, int depth, int ply, bool i
     int k1 = 2;
     int k2 = 5;
 
-    if (i <= k1 || depth <= 2  || noReduceCondition) { 
+    if (i <= k1 || depth <= 3  || noReduceCondition) { 
         return depth - 1;
     } else if (i <= k2 || reduceLessCondition) {
         return depth - 2;
