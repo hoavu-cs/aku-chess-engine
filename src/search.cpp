983ca156d5df7aa6c2648b45d4c502dacccab987
Hoa Vu <hvutrong@gmail.com>
2025-03-21 07:22:02 -0700

    revert exp

--- src/search.cpp
+++ src/search.cpp
@@ -680,30 +680,30 @@ int negamax(Board& board,
     /*--------------------------------------------------------------------------------------------
         Singular extension: If the hash move is much better than the other moves, extend the search.
     --------------------------------------------------------------------------------------------*/
-    if (found && depth >= 8 && ply <= globalMaxDepth - 1 && !mopUp) {
-        bool singularExtension = true;
-        int singularBeta = tableEval - 50; // 80 - 80 * (!isPV) * depth / 60;
-        int singularDepth = depth / 2;
-        int singularEval = -INF; 
-        int bestSingularEval = -INF;
+    // if (found && depth >= 8 && ply <= globalMaxDepth - 1 && !mopUp) {
+    //     bool singularExtension = true;
+    //     int singularBeta = tableEval - 50; // 80 - 80 * (!isPV) * depth / 60;
+    //     int singularDepth = depth / 2;
+    //     int singularEval = -INF; 
+    //     int bestSingularEval = -INF;
 
-        for (int i = 0; i < moves.size(); i++) {
-            if (moves[i].first == tableMove) {
-                continue;
-            }
-            board.makeMove(moves[i].first);
-            singularEval = -negamax(board, singularDepth, -(singularBeta + 1), -singularBeta, PV, leftMost, ply + 1, threadID);
-            board.unmakeMove(moves[i].first);
-            bestSingularEval = std::max(bestSingularEval, singularEval);
-            if (bestSingularEval >= singularBeta) {
-                singularExtension = false;
-                break;
-            }
-        }
-        if (singularExtension) {
-            depth++;
-        }
-    }
+    //     for (int i = 0; i < moves.size(); i++) {
+    //         if (moves[i].first == tableMove) {
+    //             continue;
+    //         }
+    //         board.makeMove(moves[i].first);
+    //         singularEval = -negamax(board, singularDepth, -(singularBeta + 1), -singularBeta, PV, leftMost, ply + 1, threadID);
+    //         board.unmakeMove(moves[i].first);
+    //         bestSingularEval = std::max(bestSingularEval, singularEval);
+    //         if (bestSingularEval >= singularBeta) {
+    //             singularExtension = false;
+    //             break;
+    //         }
+    //     }
+    //     if (singularExtension) {
+    //         depth++;
+    //     }
+    // }
 
     /*--------------------------------------------------------------------------------------------
         Evaluate moves.
