02a4547bb9b42d0de76609aa39057e8955359c31
Hoa Vu <hvutrong@gmail.com>
2025-04-30 18:48:20 -0700

    spsa

--- src/parameters.hpp
+++ src/parameters.hpp
@@ -2,20 +2,20 @@
 
 int historyLMR = 7882;
 
-int rfpScale = 55;
+int rfpScale = 59;
 int rfpImproving = 2;
-int rfpDepth = 9;
+int rfpDepth = 8;
 
 int singularDepth = 7;
 int singularTableReduce = 4;
-int singularReduceFactor = 4;
+int singularReduceFactor = 3;
 
 int lmpDepth = 6;
 int lmpCoeff0 = 4;
 int lmpCoeff2 = 2;
 int lmpCoeff3 = 1;
 
-int histCoeff0 = 1266;
+int histCoeff0 = 1491;
 int histCoeff1 = 2310;
 
 int seeCoeff1 = 105;
@@ -23,12 +23,12 @@ int seeDepth = 14;
 
 int fpDepth = 9;
 int fpCoeff0 = 77;
-int fpCoeff1 = 86;
+int fpCoeff1 = 82;
 int fpImprovingCoeff = 110;
 
-int maxHistory = 17613;
+int maxHistory = 15959;
 int maxCaptureHistory = 4562;
 
 int deltaCoeff2 = 3;
 int deltaCoeff1 = 0;
-int deltaCoeff0 = 3;
+int deltaCoeff0 = 2;
