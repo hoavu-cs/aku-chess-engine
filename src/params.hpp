f0a56ea4ac6006faee7505f8d12c58f0cc482e7f
Hoa Vu <hvutrong@gmail.com>
2025-05-11 18:15:44 -0700

    params file

---
+++ src/params.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+extern int rfpDepth;
+extern int rfpC1;
+extern int rfpC2;
+extern int rfpC3;
+
+extern int fpDepth;
+extern int fpC1;
+extern int fpC2;
+extern int fpC3;
+
+extern int lmpDepth;
+extern int lmpC1;
