8636e8794ad25418b30da06040c92ee8fdb2a050
Hoa Vu <hvutrong@gmail.com>
2025-05-14 01:23:35 -0700

    clean

--- training/nnue_training/src/main.rs
+++ training/nnue_training/src/main.rs
@@ -103,7 +103,7 @@ use bullet_lib::{
     },
 };
 
-const HIDDEN_SIZE: usize = 1024;
+const HIDDEN_SIZE: usize = 768;
 const SCALE: i32 = 400;
 const QA: i16 = 255;
 const QB: i16 = 64;
@@ -122,16 +122,16 @@ fn main() {
     //trainer.load_from_checkpoint("checkpoints_may/simple1024_v3-80/");
 
     let schedule = TrainingSchedule {
-        net_id: "simple1024_v4".to_string(),
+        net_id: "simple768_v7".to_string(),
         eval_scale: SCALE as f32,
         steps: TrainingSteps {
             batch_size: 16_384,
             batches_per_superbatch: 6104,
             start_superbatch: 1,
-            end_superbatch: 100,
+            end_superbatch: 50,
         },
         wdl_scheduler: wdl::ConstantWDL { value: 0.00 },
-        lr_scheduler: lr::StepLR { start: 0.001, gamma: 0.1, step: 25 },
+        lr_scheduler: lr::StepLR { start: 0.001, gamma: 0.1, step: 18 },
         save_rate: 5,
     };
 
