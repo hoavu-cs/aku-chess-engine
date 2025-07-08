6554fb1bcd91cecd4d73658f5031ebd3d1c5d74c
hoavu-cs <hvutrong.gmail.com>
2025-07-08 00:25:30 -0700

    new test

--- src/search_experiment.cpp
+++ src/search_experiment.cpp
@@ -748,7 +748,7 @@ int negamax(Board& board, int depth, int alpha, int beta, std::vector<Move>& PV,
         }
 
         extensions = std::clamp(extensions, 0, 2); 
-        next_depth = std::min(next_depth + extensions, (3 + root_depth) - ply - 1);
+        next_depth = std::min(next_depth + extensions, (2 * root_depth) - ply - 1);
 
         // common conditions for pruning
         bool can_prune = !in_check && !is_promotion_threat && i > 0 && !mopup_flag && !is_pv && !tt_is_pv;
